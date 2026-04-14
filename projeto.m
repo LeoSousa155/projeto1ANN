d7ddf8d46f0bea5f14d344f1645e1c89377c555b
LeoSousa155 <leonardodavid.dev@gmail.com>
2026-04-14 12:04:07 +0100

    Contagem de linhas com valores em falta e adição de secções no script para cada parte do projeto

--- projeto.m
+++ projeto.m
@@ -2,15 +2,37 @@
 clc
 clearvars
 
+%% 1) Import Data and Exploration
+
 % Carregar o dataset
 data = readmatrix("Gold_data.csv");
-size(data)
 
 % a função head mostra as primeiras 8 linhas dos dados selecionados
-% para mudar a visualização da tabela usamos:
-% >> format short g (formato curto, mais comum)
-% >> format long g  (formato longo com mais precisão decimal)
+    % para mudar a visualização da tabela usamos:
+    % >> format short g (formato curto, mais comum)
+    % >> format long g  (formato longo com mais precisão decimal)
+size(data)
 head(data)
 
+%% 2) Data Cleaning and Preprocessing
+
 data = data(:, 2:8);  % remover a coluna de valores nulos
+
 head(data)
+num_linhas_nan = sum(any(isnan(data), 2)) % nº de linhas com valores em falta
+
+
+%% 3) Exploratory Data Analysis (EDA)
+
+
+%% 4) Feature Transformation Tecniques
+
+
+%% 5) Feature Selection Methods
+
+
+%% 6) Feature Engineering
+
+
+%% 7) Comparative Analysis
+
